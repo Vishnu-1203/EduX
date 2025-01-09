6cb813d5ee3a985c9bc1846c54be194562aa2c38
Melvin Davis <melvindavis2003@gmail.com>
2025-01-09 10:53:25 +0530

    Update courses.jsx

--- src/screens/dashboardComponents/courses.jsx
+++ src/screens/dashboardComponents/courses.jsx
@@ -1,38 +1,42 @@
-import React from "react";
-import {View,Text,ScrollView,TouchableOpacity,StyleSheet} from "react-native";
-export default function Courses({navigation}){
-    return (<ScrollView style={{flex:1}} >
-        <View style={styles.container}s>
-        <TouchableOpacity style={styles.courses} onPress={()=>navigation.navigate("Course1")}>
-            <Text style={styles.innerText}>Course 1</Text>
+import React from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+export default function Courses({navigation}) {
+  return (
+    <ScrollView style={{flex: 1}}>
+      <View style={styles.container} s>
+        <TouchableOpacity
+          style={styles.courses}
+          onPress={() => navigation.navigate('Course1')}>
+          <Text style={styles.innerText}>Course 1</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.courses}>
-            <Text style={styles.innerText}>Course 2</Text>
+          <Text style={styles.innerText}>Course 2</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.courses}>
-            <Text style={styles.innerText}>Course 3</Text>
+          <Text style={styles.innerText}>Course 3</Text>
         </TouchableOpacity>
-        </View>
-
-        </ScrollView>)
-
-
-    }
-const styles=StyleSheet.create({
-container:{},
-
-courses:{
-    margin:"2%",
-    backgroundColor:"grey",
-    borderRadius:20,
-    height:150,
-    justifyContent:"center",
-    alignItems:"center"
-},
-innerText:{
-    fontSize:25
-
+      </View>
+    </ScrollView>
+  );
 }
+const styles = StyleSheet.create({
+  container: {},
 
-
-})
+  courses: {
+    margin: '2%',
+    backgroundColor: 'grey',
+    borderRadius: 20,
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerText: {
+    fontSize: 25,
+  },
+});
